26461c1cfb82bcb210ed010b952eb41fe9e15ead
Mikel <mikelfuentes@opendeusto.es>
2021-12-12 01:12:21 +0100

    Añadido tienda y retoques menores a la pantalla principal

---
+++ Nintenfox/Assets/Scripts/GiradorComida.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiradorComida : MonoBehaviour
+{
+    public GameObject manzana;
+    public GameObject jamon;
+    public GameObject hamburguesa;
+
+
+    private bool spining = false;
+    private Vector3 manzanaTemp;
+    private Vector3 jamonTemp;
+    private Vector3 hamburguesaTemp;
+    private string direction;
+    public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spining == true)
+        {
+            
+            if (direction == "L")
+            {
+                //Debug.Log("update");
+
+                manzana.transform.position = Vector3.Lerp(manzana.transform.position, jamonTemp,  moveSpeed * Time.deltaTime);
+
+                jamon.transform.position = Vector3.Lerp(jamon.transform.position, hamburguesaTemp, moveSpeed * Time.deltaTime);
+
+                hamburguesa.transform.position = Vector3.Lerp(hamburguesa.transform.position, manzanaTemp,  moveSpeed * Time.deltaTime);
+                //Debug.Log(Vector3.Distance(manzana.transform.position, jamonTemp));
+                if (0.001f > Vector3.Distance(manzana.transform.position, jamonTemp))
+                {
+                    
+                    spining = false;
+                }
+
+
+            }
+            else if (direction == "R")
+            {
+                manzana.transform.position = Vector3.Lerp(manzana.transform.position, hamburguesaTemp, moveSpeed * Time.deltaTime);
+
+                jamon.transform.position = Vector3.Lerp(jamon.transform.position, manzanaTemp, moveSpeed * Time.deltaTime);
+
+                hamburguesa.transform.position = Vector3.Lerp(hamburguesa.transform.position, jamonTemp, moveSpeed * Time.deltaTime);
+                
+                //Debug.Log(Vector3.Distance(manzana.transform.position, hamburguesaTemp));
+                if (0.001f > Vector3.Distance(manzana.transform.position, hamburguesaTemp))
+                {
+                    spining = false;
+                }
+
+            } 
+        } 
+    }
+
+    public void spinR()
+    {
+        
+        direction = "R";
+        if (spining == false)
+        {
+            manzanaTemp = new Vector3(0,0,0) + manzana.transform.position;
+           
+            jamonTemp = new Vector3(0, 0, 0) + jamon.transform.position;
+            
+            hamburguesaTemp = new Vector3(0, 0, 0) + hamburguesa.transform.position;
+           
+            spining = true;       
+        }
+    }
+
+    public void spinL()
+    {
+        
+        direction = "L";
+        if (spining == false)
+        {
+            manzanaTemp = new Vector3(0, 0, 0) + manzana.transform.position;
+
+            jamonTemp = new Vector3(0, 0, 0) + jamon.transform.position;
+
+            hamburguesaTemp = new Vector3(0, 0, 0) + hamburguesa.transform.position;
+
+            spining = true;
+        }
+    }
+
+}
---
+++ Nintenfox/Assets/Scripts/MiradorPortal.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiradorPortal : MonoBehaviour
+{
+    public Transform zorro;
+    private bool mira = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mira == true)
+        {
+            transform.LookAt(zorro);
+            transform.localRotation = Quaternion.Euler(-90f, transform.localRotation.eulerAngles.y, 0f);
+            mira = false;
+        }
+
+    }
+
+    public void looker()
+    {
+        Debug.Log("patatap");
+        mira = true;
+        //transform.LookAt(zorro);
+        //transform.localRotation = Quaternion.Euler(-180f, transform.localRotation.eulerAngles.y-180, 0f);
+    }
+}
